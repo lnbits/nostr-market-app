c2556221313a66ece15e9f80055457bfb7c66dda
Vlad Stan <stan.v.vlad@gmail.com>
2023-08-10 20:14:51 +0300

    feat: add `UserProfile` component

--- src/components/MarketConfig.vue
+++ src/components/MarketConfig.vue
@@ -204,32 +204,10 @@
                     v-for="pubkey of marketData.opts.merchants"
                     :key="pubkey"
                   >
-                    <q-item-section avatar>
-                      <q-avatar>
-                        <img
-                          v-if="merchantProfile(pubkey)?.picture"
-                          :src="merchantProfile(pubkey).picture"
-                        />
-                        <div v-else :innerHTML="pubkeyAvatar(pubkey)">
-                          <!-- {{ }} -->
-                        </div>
-                      </q-avatar>
-                    </q-item-section>
-                    <q-item-section class="q-mt-sm">
-                      <q-item-label
-                        ><strong>{{
-                          merchantProfile(pubkey)?.name ||
-                          merchantProfile(pubkey)?.display_name ||
-                          pubkeyAlias(pubkey)
-                        }}</strong></q-item-label
-                      >
-                      <q-item-label>
-                        <div class="text-caption text-grey ellipsis-2-lines">
-                          <p>{{ pubkey }}</p>
-                        </div>
-                      </q-item-label>
-                      <q-tooltip>{{ pubkey }}</q-tooltip>
-                    </q-item-section>
+                    <user-profile
+                      :pubkey="pubkey"
+                      :profiles="profiles"
+                    ></user-profile>
                     <q-item-section side>
                       <q-btn
                         size="12px"
@@ -342,18 +320,12 @@
 
 <script>
 import { defineComponent } from "vue";
-import {
-  uniqueNamesGenerator,
-  adjectives,
-  colors,
-  animals,
-} from "unique-names-generator";
-
-import { toSvg } from "jdenticon";
+import UserProfile from "./UserProfile.vue";
 
 export default defineComponent({
   name: "EssentialLink",
   props: ["market", "profiles", "relays-data", "read-notes"],
+  components: { UserProfile },
 
   data: function () {
     return {
@@ -474,18 +446,6 @@ export default defineComponent({
           error: null,
         }
       );
-    },
-    pubkeyAlias(pubkey) {
-      return uniqueNamesGenerator({
-        dictionaries: [adjectives, animals, colors],
-        length: 2,
-        separator: " ",
-        style: "capital",
-        seed: pubkey,
-      });
-    },
-    pubkeyAvatar(pubkey) {
-      return toSvg(pubkey, 100);
     },
   },
   created: async function () {
