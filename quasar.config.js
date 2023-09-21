abd350a26c4ae43cb6797b597d7c8d6bbc185e46
dni ⚡ <office@dnilabs.com>
2023-09-21 13:49:55 +0200

    [fix] base path (#6)

--- quasar.config.js
+++ quasar.config.js
@@ -87,7 +87,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       // config: { staticPath: "/nostrmarket/static/market/" },
-      config: { staticPath: "/" },
+      config: { staticPath: "" },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
