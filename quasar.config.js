5966a86e7585e34e48d5b80ecb2dd9992ccc443b
Patrick Mulligan <patjmulligan@gmail.com>
2024-12-14 14:44:34 -0600

    update dependencie & fix quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -8,9 +8,9 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
-const { configure } = require("quasar/wrappers");
+import { configure } from "quasar/wrappers";
 
-module.exports = configure(function (/* ctx */) {
+export default configure(function (/* ctx */) {
   return {
     eslint: {
       // fix: true,
